e5fe62caddfe0f2894c92d2784cddad9526f62bf
Jonathan <20230174@ricaldone.edu.sv>
2025-07-30 11:05:07 -0600

    fixing app mistake 1

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ import employeesRoutes from "./src/routes/employees.js";
 import historyRawMaterialsRoutes from "./src/routes/historyRawMaterials.js";
 import productPriceHistoryRoutes from "./src/routes/productPriceHistory.js";
 import collectionsRoutes from "./src/routes/collections.js";
-import shoppingCartRoutes from "./src/routes/shoppingcart.js";
+import shoppingCartRoutes from "./src/routes/shoppingCart.js";
 import salesOrderRoutes from "./src/routes/salesOrder.js";
 import productCategoriesRoutes from "./src/routes/productCategories.js";
 import productsRoutes from "./src/routes/products.js";
